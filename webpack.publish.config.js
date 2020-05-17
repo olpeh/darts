0e1f789ed5dca1070ce51c1cbf3ce9f8b7bcf2c1
Olavi Haapala <olavi.haapala@futurice.com>
2020-05-17 12:45:53 +0300

    Change public path to /

--- webpack.publish.config.js
+++ webpack.publish.config.js
@@ -1,42 +1,42 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
+  mode: 'production',
+  entry: './src/index.js',
   output: {
-    path: __dirname + "/dist/",
-    filename: "js/bundle.js",
-    publicPath: "/dartspillet/",
+    path: __dirname + '/dist/',
+    filename: 'js/bundle.js',
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
           },
         },
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          outputPath: "/images",
+          outputPath: '/images',
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "src/index.html",
+      filename: 'index.html',
+      template: 'src/index.html',
     }),
   ],
   watch: false,
